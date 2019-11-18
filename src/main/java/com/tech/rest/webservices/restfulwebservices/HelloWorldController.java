2bd5187132ac322124dbe270fdd07cf5b6f4ea58
Apple <Apple@MacFreakss-MacBook.local>
2019-11-18 22:05:56 +0530

    Initial commit

---
+++ src/main/java/com/tech/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.tech.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello-World My First Spring Boot program";
+    }
+}
